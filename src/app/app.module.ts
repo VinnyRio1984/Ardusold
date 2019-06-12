1f05ff6c1b8130d5f654fec517b0b9f8a4c43192
Vinicius de Araujo Diniz <vinicius.diniz@bndes.gov.br>
2019-06-12 09:55:24 -0300

    criação do component empreegado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpregadosComponent } from './empregados/empregados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpregadosComponent
   ],
   imports: [
     BrowserModule,
