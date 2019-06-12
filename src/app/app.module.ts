37120497ee8ccb9168fd284c84e32a5ea2d08a43
Vinicius de Araujo Diniz <vinicius.diniz@bndes.gov.br>
2019-06-12 10:35:33 -0300

    adicionando components ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpregadosComponent } from './empregados/empregados.component';
+import { MaquinasComponent } from './maquinas/maquinas.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ServicosComponent } from './servicos/servicos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpregadosComponent
+    EmpregadosComponent,
+    MaquinasComponent,
+    ClientesComponent,
+    ServicosComponent
   ],
   imports: [
     BrowserModule,
