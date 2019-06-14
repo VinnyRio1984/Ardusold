d73a2f9e55113b667a0d17e595d5036112aefe3c
Vinicius de Araujo Diniz <vinicius.diniz@bndes.gov.br>
2019-06-14 11:39:03 -0300

    Alterando o Nome do Projeto de ArduSold -> ArduSolda

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ArduSold'`, () => {
+  it(`should have as title 'ArduSolda'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ArduSold');
+    expect(app.title).toEqual('ArduSolda');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ArduSold!');
+    expect(compiled.querySelector('h1').textContent).toContain('ArduSolda!');
   });
 });
